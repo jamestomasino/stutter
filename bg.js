cf6b29f412af98c5d19b2ab674fa570e77d93c61
James Tomasino <james@tomasino.org>
2019-01-30 22:32:43 -0500

    placeholder files from Read

--- bg.js
+++ bg.js
@@ -1,1 +1,38 @@
-// Put all the javascript code here, that you want to execute in background.
+/* global browser */
+function onContextCLick (info) {
+  browser.tabs.query({
+    'active': true,
+    'currentWindow': true
+  }, function (tabs) {
+    browser.tabs.sendMessage(tabs[0].id, {
+      'functiontoInvoke': 'readSelectedText',
+      'selectedText': info.selectionText
+    })
+  })
+}
+
+function onIconClick () {
+  browser.tabs.query({
+    'active': true,
+    'currentWindow': true
+  }, function (tabs) {
+    browser.tabs.sendMessage(tabs[0].id, {
+      'functiontoInvoke': 'readFullPage'
+    })
+  })
+}
+
+// Write this in an expandable way in case we want to move beyond selection
+var contexts = ['selection']
+for (var i = 0; i < contexts.length; i++) {
+  var context = contexts[i]
+  var title = 'Read Selected Text'
+  browser.contextMenus.create({
+    'title': title,
+    'contexts': [context],
+    'onclick': onContextCLick
+  })
+}
+
+// Handle clicking on the browser icon
+browser.browserAction.onClicked.addListener(() => { onIconClick() })
--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Put all the javascript code here, that you want to execute after page load.
+/* global browser */
+
+const Stutter = require('./lib/stutter')
+
+var stutter // stutter Object
+var stutterOptions = {
+  'wpm': 700,
+  'slowStartCount': 5,
+  'sentenceDelay': 2.5,
+  'otherPuncDelay': 1.5,
+  'shortWordDelay': 1.3,
+  'longWordDelay': 1.4
+}
+
+browser.extension.onMessage.addListener(request => {
+  switch (request.functiontoInvoke) {
+    case 'stutterSelectedText':
+      playStutter(request.selectedText)
+      break
+    case 'stutterFullPage':
+      playStutter('This is just a test.')
+      break
+    default:
+      break
+  }
+})
+
+function playStutter (text) {
+  stutter = new Stutter(stutterOptions)
+  stutter.setText(text)
+  stutter.play()
+}
