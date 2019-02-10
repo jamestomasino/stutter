9c65db69bcc55ad67747ab6327bbee0307fe91bc
James Tomasino <james@tomasino.org>
2019-02-10 01:20:37 -0500

    beginning work on options panel

--- options/script.js
+++ options/script.js
@@ -1,1 +1,38 @@
-document.getElementById('myHeading').style.color = 'red'
+/* global browser */
+
+/* TODO: storage changes do not publish events.
+ * we must fire a message to inform the rest of the
+ * extension when setting changes have been made.
+ */
+
+var options = {
+  wpm: 700,
+  slowStartCount: 5,
+  sentenceDelay: 2.5,
+  otherPuncDelay: 1.5,
+  shortWordDelay: 1.3,
+  longWordDelay: 1.4
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  browser.storage.sync.get('stutterOptions').then(result => {
+    if (result.stutterOptions) {
+      for (let key in options) {
+        document.getElementById(key).value = result.stutterOptions[key] || options[key]
+      }
+    }
+  },
+  error => {
+    console.log(`Error: ${error}`)
+  })
+
+  document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault()
+    for (let key in options) {
+      options[key] = document.getElementById(key).value
+    }
+    browser.storage.sync.set({
+      stutterOptions: options
+    })
+  })
+})
--- src-content/lib/stutterOptions.js
+++ src-content/lib/stutterOptions.js
@@ -1,11 +1,30 @@
+/* global browser */
+
 export default class StutterOptions {
   constructor () {
+    // Default settings
     this._wpm = 700
     this._slowStartCount = 5
     this._sentenceDelay = 2.5
     this._otherPuncDelay = 1.5
     this._shortWordDelay = 1.3
     this._longWordDelay = 1.4
+
+    // TODO: refactor this with the options panel
+
+    // Attempt to use browser sync for options
+    var getting = browser.storage.sync.get('color')
+    getting.then(result => {
+      if (result && result.stutterOptions) {
+        this._wpm = result.stutterOptions.wpm || this._wpm
+        this._slowStartCount = result.stutterOptions.slowStartCount || this._slowStartCount
+        this._sentenceDelay = result.stutterOptions.sentenceDelay || this._sentenceDelay
+        this._otherPuncDelay = result.stutterOptions.otherPuncDelay || this._otherPuncDelay
+        this._shortWordDelay = result.stutterOptions.shortWordDelay || this._shortWordDelay
+        this._longWordDelay = result.stutterOptions.longWordDelay || this._longWordDelay
+      }
+    })
+
     this._delay = 1 / (this._wpm / 60) * 1000
   }
 
