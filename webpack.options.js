2d4f3d027e5cbf911775ef416df94e6c34350082
James Tomasino <james@tomasino.org>
2019-02-10 10:08:39 -0500

    options panel has separate webpack build as well

---
+++ webpack.options.js
@@ -0,0 +1,41 @@
+var path = require('path')
+
+module.exports = {
+  'mode': 'development',
+  'entry': './src-options/index',
+  'output': {
+    'path': path.resolve(__dirname, './options'),
+    'filename': 'index.js'
+  },
+  'devtool': 'source-map',
+  'module': {
+    'rules': [
+      {
+        'enforce': 'pre',
+        'test': /src-options\/.*\.js$/,
+        'exclude': /node_modules/,
+        'use': 'eslint-loader'
+      },
+      {
+        'test': /src-options\/.*\.js$/,
+        'exclude': /node_modules/,
+        'use': {
+          'loader': 'babel-loader',
+          'options': {
+            'presets': [
+              '@babel/preset-env'
+            ],
+          }
+        }
+      },
+      {
+        'test': /src-options\/.*\.scss$/,
+        'use': [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+}
