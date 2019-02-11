8d434c5c80889386e5132abfcf8e08f7ae6eff77
James Tomasino <james@tomasino.org>
2019-02-10 19:28:39 -0500

    better text highlighting of center char

--- src-content/lib/ui.js
+++ src-content/lib/ui.js
@@ -4,18 +4,21 @@ class UI {
   constructor () {
     this.template = `
     <div class="__stutter_text">
-      <span class="__stutter_left"></span><span class="__stutter_right"></span>
+      <span class="__stutter_left"></span>
+      <span class="__stutter_right"><span class="__stutter_center"></span><span class="__stutter_remainder"></span></span>
     </div>`
     this.holder = document.createElement('div')
     this.holder.classList.add('__stutter')
     this.holder.innerHTML = this.template
     this.left = this.holder.getElementsByClassName('__stutter_left')[0]
-    this.right = this.holder.getElementsByClassName('__stutter_right')[0]
+    this.center = this.holder.getElementsByClassName('__stutter_center')[0]
+    this.remainder = this.holder.getElementsByClassName('__stutter_remainder')[0]
   }
 
   show (word) {
     this.left.textContent = word.val.substr(0, word.index)
-    this.right.textContent = word.val.substr(word.index)
+    this.center.textContent = word.val.substr(word.index, 1)
+    this.remainder.textContent = word.val.substr(word.index + 1)
   }
 
   hide () {
