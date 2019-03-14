3a91e5cc487a1ea154b09758459381f5b2969f4c
James Tomasino <james@tomasino.org>
2019-03-14 18:09:55 -0400

    dont scroll to the top anymore

--- src-content/lib/ui.js
+++ src-content/lib/ui.js
@@ -72,8 +72,6 @@ export default class UI extends EventEmitter {
   }
 
   resume () {
-    // move to top of screen to see player
-    window.scrollTo(0, 0)
     // hide pause overlay
     this.holder.classList.remove('__stutter_paused')
   }
