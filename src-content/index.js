974c7e186a10da40fd0cd0fd7000d9e99bdc9632
James Tomasino <james@tomasino.org>
2025-02-19 10:14:06 +0000

    removes unnecessary password text selection

--- src-content/index.js
+++ src-content/index.js
@@ -58,7 +58,7 @@ function getSelectionText () {
   var text = ''
   var activeEl = document.activeElement
   var activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null
-  if ((activeElTagName === 'textarea') || ((activeElTagName === 'input' && /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) && (typeof activeEl.selectionStart === 'number'))) {
+  if (activeElTagName === 'textarea') {
     text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd)
   } else if (window.getSelection) {
     text = window.getSelection().toString()
